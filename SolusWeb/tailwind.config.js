de68000555aa57729b4660604dff12a06bdc5f06
AvogatoWizardWhisker <babis_abdul@hotmail.fr>
2024-12-23 13:27:05 +0100

    Added DaisyUI plugin

--- SolusWeb/tailwind.config.js
+++ SolusWeb/tailwind.config.js
@@ -1,13 +1,12 @@
-const colors = require('tailwindcss/colors');
-module.exports = {
-    content:
-        [
-            './**/*.razor',
-            './wwwroot/index.html',
-            './wwwroot/**/*.css',
-        ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+import colors from 'tailwindcss/colors';
+export const content = [
+    './**/*.razor',
+    './wwwroot/index.html',
+    './wwwroot/**/*.css',
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [
+    require('daisyui'),
+];
